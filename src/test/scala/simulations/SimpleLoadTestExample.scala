31944484485851f8d2b954ad0add535e960b9198
Kostiantyn Teltov <dneprokos@gmail.com>
2020-06-05 14:26:34 +0300

    Simple Load test was added

---
+++ src/test/scala/simulations/SimpleLoadTestExample.scala
@@ -0,0 +1,20 @@
+package simulations
+
+import baseConfig.BaseSimulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class SimpleLoadTestExample extends BaseSimulation {
+
+  // 1 HTTP Protocol step builder was moved to base class
+
+  // 2 Scenario Definition
+  val scn = scenario("Simple Test")
+    .exec(http("Get All Games")
+      .get("videogames"))
+
+  // 3 Load Scenario
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+}
